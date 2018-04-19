b94f27ef6c478a3d16b506a797bd911f45df712c
Léonard <leonard.assouline@ens-lyon.fr>
2018-04-19 17:46:37 +0200

    some modifications to gui_skeleton.cpp. It still doesn't compile, I haven't figured out why yet

--- GUI/gui_skeleton.cpp
+++ GUI/gui_skeleton.cpp
@@ -8,7 +8,7 @@ using namespace cv;
 
 int main(int argc, char const *argv[]) {
   if (argc != 2) {
-    cout << "Use: Create_mask <image>" << "\n" ;
+    std::cout << "Use: Create_mask <image>" << "\n" ;
     return 1 ;
   }
   Mat image = imread(argv[1], IMREAD_COLOR) ;
@@ -17,18 +17,22 @@ int main(int argc, char const *argv[]) {
   int w = image.rows ;
   Mat masque(w, h, CV_8UC3, Scalar(0,0,0)) ;
   namedWindow(WINDOW_NAME);
-  init(WINDOW_NAME) ;
+  cvui::init(WINDOW_NAME) ;
   int cursor_radius = 2 ; //How many pixels are selected when the mask is drawn ?
+  Vec3b color = image.at<Vec3b>(Point(0, 0));
+  color[0] = 255 ;
+  color[1] = 255 ;
+  color[2] = 255 ;
   while (true) {
     // cvui::window(frame, 10, 50, 180, 180, "Settings");
     cvui::image(frame, 10, 10, image);
     if (cvui::mouse(cvui::LEFT_BUTTON, cvui::IS_DOWN)) {
       cv::Point cursor = cvui::mouse() ;
-      for (int i = -1*cursor_radius; i <= cursor_radius, i++) {
+      for (int i = -1*cursor_radius; i <= cursor_radius; i++) {
         if ((cursor.x + i) <= w && (cursor.y + i) <= h)
-          masque.at<Vec3b>(Point(cursor.x + i,cursor.y + i)) = Scalar(255, 255, 255) ;
+          masque.at<Vec3b>(Point(cursor.x + i,cursor.y + i)) = color ;
         if ((cursor.x - i) <= w && (cursor.y + i) <= h)
-          masque.at<Vec3b>(Point(cursor.x - i,cursor.y + i)) = Scalar(255, 255, 255) ;
+          masque.at<Vec3b>(Point(cursor.x - i,cursor.y + i)) = color ;
 
       }
      }
